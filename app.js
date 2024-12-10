15259d90d5d256f8046c33dca1499e8ba214b0df
kyoko nozaki <nozaki_kyoko@waku-2.com>
2024-12-10 12:09:16 +0900

    public→src作り直し

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'src')));
 
 app.get('/', (_req, res) => {
   res.render('index');
