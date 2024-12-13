f1a2b0f6ad6a352e9f1e6a295566e7aa08d31db2
kyoko nozaki <apricotn18@gmail.com>
2024-12-13 16:41:50 +0900

    src→public作り直し

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (_req, res) => {
   res.render('index');
